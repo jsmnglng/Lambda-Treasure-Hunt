cf4dc33614f50cae1f0849cb25d808bde3ca3f3c
jsmnglng <jsmn.glng@gmail.com>
2019-02-04 15:10:39 -0800

    Manually tested going through each room in the map

--- treasure_hunt/src/components/MapTraversal.js
+++ treasure_hunt/src/components/MapTraversal.js
@@ -2,33 +2,88 @@ import React, { Component } from "react";
 import axios from "axios";
 
 const baseURL = "https://lambda-treasure-hunt.herokuapp.com/api/adv";
+const config = {
+  headers: {
+    Authorization: `Token ${process.env.REACT_APP_API_KEY}`
+  }
+};
 
 class MapTraversal extends Component {
-  state = {};
+  state = {
+    graph: { 0: { n: "?", s: "?", e: "?", w: "?" } },
+    room_id: 0,
+    coordinates: "",
+    exits: [],
+    input: ""
+  };
 
   componentDidMount() {
     this.init();
   }
 
   init = () => {
-    const config = {
-      headers: {
-        Authorization: `Token ${process.env.REACT_APP_API_KEY}`
-      }
-    };
-
     axios
       .get(`${baseURL}/init/`, config)
       .then(res => {
-        console.log(res.data);
+        console.log("GET res.data", res.data);
+        this.setState({
+          room_id: res.data.room_id,
+          coordinates: res.data.coordinates,
+          exits: res.data.exits
+        });
       })
-      .catch(err => {
-        console.log(err);
-      });
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    this.setState({ input: event.target.value });
+  };
+
+  moveSubmit = e => {
+    e.preventDefault();
+    const { input } = this.state;
+    const data_input = { direction: input };
+
+    switch (input.toLowerCase()) {
+      case "n":
+      case "s":
+      case "e":
+      case "w":
+        axios
+          .post(`${baseURL}/move`, data_input, config)
+          .then(res => {
+            console.log("POST res.data", res.data);
+            this.setState({
+              room_id: res.data.room_id,
+              coordinates: res.data.coordinates,
+              exits: res.data.exits,
+              input: ""
+            });
+          })
+          .catch(err => console.log(err));
+        return;
+      default:
+        return;
+    }
   };
 
   render() {
-    return <h1>Map Traversal</h1>;
+    const { room_id, exits, coordinates, input } = this.state;
+    const room_exits = exits.join(" ").toUpperCase();
+
+    return (
+      <div>
+        <h1>Room {room_id}</h1>
+        <h2>Coordinates: {coordinates}</h2>
+        <h2>Exits: {room_exits}</h2>
+
+        <form onSubmit={this.moveSubmit}>
+          <label>Move to: </label>
+          <input type="text" value={input} onChange={this.handleInputChange} />
+          <button type="submit">Go</button>
+        </form>
+      </div>
+    );
   }
 }
 
---
+++ treasure_hunt/src/components/Room.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./components.css";
+
+const Room = () => {
+  return <div className="square" />;
+};
+
+export default Room;
