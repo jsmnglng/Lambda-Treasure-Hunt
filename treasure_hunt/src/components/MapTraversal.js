ab0b5b33dadb445e59ee80715ad09f7e39c3726f
jsmnglng <jsmn.glng@gmail.com>
2019-02-06 09:54:07 -0800

    Fixed some more errors

--- treasure_hunt/src/components/MapTraversal.js
+++ treasure_hunt/src/components/MapTraversal.js
@@ -7,6 +7,7 @@ const config = {
     Authorization: `Token ${process.env.REACT_APP_API_KEY}`
   }
 };
+// const graph = { 0: { n: "?", s: "?", e: "?", w: "?" } };
 
 class MapTraversal extends Component {
   constructor(props) {
@@ -17,16 +18,20 @@ class MapTraversal extends Component {
       coordinates: "",
       exits: [],
       cooldown: 0,
-      input: ""
+      input: "",
+      traversalPath: []
+      // unexplored_exits: []
     };
-    this.graph = {};
-    this.moves = {};
+    this.graph = { 0: { n: "?", s: "?", e: "?", w: "?" } };
+    // this.moves = {};
   }
 
   componentDidMount() {
     if (localStorage.hasOwnProperty("graph")) {
       let value = JSON.parse(localStorage.getItem("graph"));
-      this.setState({ graph: value });
+      console.log("VALUE", value);
+      this.graph = value;
+      console.log("THIS.GRAPH CDM", this.graph);
     }
     this.getLocation();
   }
@@ -87,10 +92,10 @@ class MapTraversal extends Component {
     e.preventDefault();
     const { input } = this.state;
     const data_input = { direction: input };
-    const traversalPath = [];
+    // const traversalPath = [];
     // const stack = [];
     // const newGraph = { ...graph };
-    this.graph[this.state.room_id] = { n: "?", s: "?", e: "?", w: "?" };
+    // this.graph[this.state.room_id] = { n: "?", s: "?", e: "?", w: "?" };
 
     let current_room_exits = this.graph[this.state.room_id];
     console.log("CURRENT ROOM EXITS", current_room_exits);
@@ -105,9 +110,11 @@ class MapTraversal extends Component {
 
     let exit = input;
     if (unexplored_exits) {
+      console.log("UNEXPLORED EXITS", unexplored_exits);
       let prev_room_id = this.state.room_id;
       if (["n", "s", "e", "w"].includes(exit)) {
-        traversalPath.push(exit);
+        this.state.traversalPath.push(exit);
+        console.log("TRAVERSAL PATH", this.state.traversalPath);
         // stack.push(exit);
         axios
           .post(`${baseURL}/move`, data_input, config)
@@ -122,21 +129,22 @@ class MapTraversal extends Component {
               input: ""
             });
             console.log("POST res.data AFTER", res.data);
-
-            this.state.exits.forEach(exit => {
-              this.moves[exit] = "?";
+            console.log("RES.DATA.EXITS", res.data.exits);
+            const moves = {};
+            res.data.exits.forEach(exit => {
+              moves[exit] = "?";
             });
-            console.log("MOVES", this.moves);
+            console.log("MOVES", moves);
             // console.log("ROOM ID", room_id);
-            console.log("RAW ROOM ID", this.state.room_id);
+            console.log("RAW ROOM ID", res.data.room_id);
             // this.moves[this.oppositeDir(exit)] = prev_room_id;
             console.log("THIS.GRAPH", this.graph);
-            this.graph[prev_room_id][exit] = this.state.room_id;
-            this.graph[this.state.room_id] = { ...this.moves };
+            this.graph[prev_room_id][exit] = res.data.room_id;
+            this.graph[res.data.room_id] = moves;
             console.log("PREV ROOM ID", prev_room_id);
-            this.graph[this.state.room_id][
-              this.oppositeDir(exit)
-            ] = prev_room_id;
+            this.graph[res.data.room_id][this.oppositeDir(exit)] = prev_room_id;
+            console.log("THIS.GRAPH AFTER", this.graph);
+            localStorage.setItem("graph", JSON.stringify(this.graph));
           })
           .catch(err => console.log(err));
       }
